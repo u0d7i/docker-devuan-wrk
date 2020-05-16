bb8c738d467c3545d39f37467c573c74d1d8cbd9
u0d7i <u0d7i@users.noreply.github.com>
2020-05-16 16:47:06 +0300

    be verbose

--- scripts/mksd.sh
+++ scripts/mksd.sh
@@ -36,25 +36,34 @@ done
 cryptsetup status ${CRMAP} && abort "container is active, quitting"
 
 # dangerouus stuff starts here
+echo "+ Filling ${D}2 with random data"
+dd if=/dev/urandom of=${D}2 bs=100M status=progress
+echo "+ Formatting LUKS container on ${D}2"
 # interactive
 cryptsetup luksFormat ${D}2 || abort "luksFormat failed"
 # unattended
 # echo -n "$CPASS" | cryptsetup -q luksFormat ${D}2 -
 # or use pre-generated key
 
+echo "+ Opening LUKS container on ${D}2 as ${CRMAP}"
 cryptsetup luksOpen ${D}2 ${CRMAP} || abort "luksOpen failed"
 # unattended
 # echo -n "$CPASS" | cryptsetup luksOpen ${D}2 ${CRMAP} -
 # or use pre-generated key
 
+echo "+ dd root.img to ${CRMAP}"
 dd if=/data/root.img of=/dev/mapper/${CRMAP} status=progress
+echo "+ Resizing filesystem on ${CRMAP}"
 e2fsck -f /dev/mapper/${CRMAP}
 resize2fs -p /dev/mapper/${CRMAP}
 e2fsck -f /dev/mapper/${CRMAP}
 cryptsetup luksClose ${CRMAP}
 
+echo "+ Filling ${D}1 with random data"
 dd if=/dev/urandom of=${D}1 bs=10M status=progress
+echo "+ dd boot.img to  ${D}1"
 dd if=/data/boot.img of=${D}1 status=progress
+echo "+ Resizing filesystem on ${D}1"
 e2fsck -f ${D}1
 resize2fs -p ${D}1
 e2fsck -f ${D}1
